fc38fe43902a61c1a8ab39649a254841606591c2
ericbanker12 <ericbanker12@gmail.com>
2019-10-11 11:10:55 -0500

    update SmurfList, create actions

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const GET_SMURFS = 'GET_SMURFS'
+export const SET_SMURFS = 'SET_SMURFS'
+export const SET_ERROR = 'SET_ERROR'
+
+export const getSmurfs = () => dispatch => {
+    dispatch({type: GET_SMURFS})
+    axios.get('http://localhost:3333/smurfs')
+        .then(resp => {
+            console.log(resp)
+            // dispatch()
+        })
+        .catch(err => {
+            console.error(err)
+            // dispatch()
+        })
+}
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { getSmurfs } from '../actions'
+
+// import Smurf from './Smurf'
 
 function SmurfList(props) {
+    // API GET on mount
+    React.useEffect(()=>{
+        const dispatch = useDispatch()
+        dispatch(getSmurfs())
+    },[])
+
     // get smurfs
+    const { smurfs } = useSelector(({smurfs})=>({smurfs}))
 
     return (
         <div>
-            
+            {/* {smurfs.map(smurf => <Smurf key={smurf.id} smurf={smurf} />)} */}
         </div>
     )
 }
