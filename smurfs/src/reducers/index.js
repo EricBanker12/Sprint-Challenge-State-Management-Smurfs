d1ada94984e7f2a5dc5a8be35bea26a74d79bcb2
ericbanker12 <ericbanker12@gmail.com>
2019-10-11 10:38:34 -0500

    create reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initState = {
+    smurfs: []
+}
+
+function reducer(state = initState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
